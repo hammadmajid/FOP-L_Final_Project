69d01f79c9c3862b3f5be4d096afaa0f450c0b02
Hammad Majid <hammadmajid@proton.me>
2023-12-21 18:19:28 +0500

    create enum and struct to represent required data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+enum CipherOperation {
+  Encryption,
+  Decryption,
+};
+
+struct CipherInfo {
+  string filename;
+  unsigned short int key;
+  CipherOperation type;
+};
+
 int main() {
   cout << "Hello, world" << endl;
 
